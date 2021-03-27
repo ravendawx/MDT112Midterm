3a0c539b5667d944bbad47dfb43364370c01053b
6-077-บุญญิสา ช่วยเมือง <boonyisa.bu@mail.kmutt.ac.th>
2021-03-28 03:38:37 +0700

    ข้อ2

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+
+            Console.Write("Input a = ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Input b = ");
+            b = int.Parse(Console.ReadLine());
+
+            int x = a;
+            int y = b;
+
+            while (x != y)
+            {
+                if (x < y)
+                {
+                    x = x + a;
+                }
+                else
+                {
+                    y = y + b;
+                }
+            }
+            if (x == y)
+            {
+                Console.WriteLine("least common multiple is {0}",x);
+            }
+            else
+            {
+                Console.WriteLine("least common multiple is {0}", x);
+            }
+        }
+    }
+}
