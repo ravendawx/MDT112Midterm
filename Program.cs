e2d6d5805011c4061c7752bfd3f6b37466e2f477
6-077-บุญญิสา ช่วยเมือง <boonyisa.bu@mail.kmutt.ac.th>
2021-03-28 02:59:07 +0700

    ข้อ 2

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace midterm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+
+            Console.Write("Input a = ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Input b = ");
+            b = int.Parse(Console.ReadLine());
+
+            int x = a;
+            int y = b;
+
+            
+            if (x == y)
+            {
+                Console.WriteLine("least common multiple is {0}", x);
+            }
+            else
+            {
+                if (x < y)
+                {
+                    x = x + a;
+                    Console.WriteLine("least common multiple is {0}", x);
+                }
+            
+                else 
+                {
+                    y = y + b;
+                    Console.WriteLine("least common multiple is {0}", y);
+                }
+            }
+            }
+
+        }
+    }
+
