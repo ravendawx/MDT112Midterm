ff50b8dc49b7b83283a0fe3bdfdcb49a5df8db55
6-077-บุญญิสา ช่วยเมือง <boonyisa.bu@mail.kmutt.ac.th>
2021-04-24 15:52:11 +0700

    ข้อ7

---
+++ 3/3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            
+
+            Console.Write("Enter a positive number of box in tank one: ");
+            int tank1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter a positive number of box in tank two: ");
+            int tank2 = int.Parse(Console.ReadLine());
+            Console.Write("Enter a positive number of box in tank three: ");
+            int tank3 = int.Parse(Console.ReadLine());
+
+            
+
+            Console.Write("Player A Select the tank: ");
+            int playerApicknumberoftank = int.Parse(Console.ReadLine());
+            Console.Write("Player A Select the box: ");
+            int playerApicknumberofbox = int.Parse(Console.ReadLine());
+            Console.Write("Player B Select the tank: ");
+            int playerBpicknumberoftank = int.Parse(Console.ReadLine());
+            Console.Write("Player B Select the box: ");
+            int playerBpicknumberofbox = int.Parse(Console.ReadLine());
+
+            Random Alltank = new Random();
+            
+             
+        }
+    }
+}
---
+++ 7.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day;
+            Console.WriteLine("day = ");
+            day = int.Parse(Console.ReadLine());
+            day = ((int)DateTime.Now.DayOfWeek == 0) ? 7 : (int)DateTime.Now.DayOfWeek;
+
+            int time;
+            Console.WriteLine("time = ");
+            time = int.Parse(Console.ReadLine());
+
+            
+            string[] menu = new string[] { "Breakfast Set", "Weekend Set", "Coffee" };
+            Console.WriteLine("menu = ");
+            string Menu = Console.ReadLine();
+            foreach()
+
+            
+
+
+
+        }
+    }
+}
