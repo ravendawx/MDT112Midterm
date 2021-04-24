909c25b19a39c4f1b987c2635d11f048ce425309
6-077-บุญญิสา ช่วยเมือง <boonyisa.bu@mail.kmutt.ac.th>
2021-04-24 16:08:34 +0700

    6

---
+++ 6.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Char input;
+                input = Console.ReadLine()[0];
+                int width;
+                width = int.Parse(Console.ReadLine());
+                int height;
+                height = int.Parse(Console.ReadLine());
+                int step;
+                step = int.Parse(Console.ReadLine());
+
+                for (int a = 0; a <= step; a++)
+                {
+                    for (int b = 0; b < height; b++)
+                    {
+                        for (int c = 0; c < width * a; c++)
+                        {
+                            Console.Write(input);
+                        }
+                        Console.WriteLine("n");
+                    }
+                }
+            
+        }
+    }
+}
