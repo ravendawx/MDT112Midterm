100e5da723b054f4a62f430da20dd7be8e86044a
6-077-บุญญิสา ช่วยเมือง <boonyisa.bu@mail.kmutt.ac.th>
2021-04-30 14:35:03 +0700

    HW4 ข้อ1

---
+++ HW4/1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Pascal(int i)
+        {
+            i++;
+            for (int line = 0; line < i; line++)
+            {
+                for (int a = 0; i <= line; a++) ;
+                Console.Write(binomialcoefficient(line, i) + " ");
+                Console.WriteLine();
+            }
+        }
+        public static int binomialcoefficient(int i, int j)
+        {
+            int res = 1;
+            if (j > i - j)
+                j = i - j;
+            for (int k = 0; k < j; ++k)
+            {
+                res *= (i - k);
+                res /= (i + 1);
+            }
+
+            return res;
+        }
+
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n < 0)
+            {
+                while (n < 0)
+                {
+                    Console.WriteLine("Invalid Pascal's triangle row number.\n");
+                    n = int.Parse(Console.ReadLine());
+                }
+            }
+
+        }
+        }
+    }
