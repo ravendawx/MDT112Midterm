4efd493062022d83b4850d314257dd9a4180f23d
6-077-บุญญิสา ช่วยเมือง <boonyisa.bu@mail.kmutt.ac.th>
2021-04-30 15:02:09 +0700

    HW4 ข้อ 2

---
+++ HW4/2.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    public class Program
+    {
+        static bool isinputValid()
+        {
+            string YN = Console.ReadLine();
+            while ((YN != "Y") && (YN != "N"))
+            {
+                Console.WriteLine("Please input Y or N.");
+                YN = Console.ReadLine();
+            }
+
+            if (YN == "Y")
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        static bool IsValidSequence(string halfDNASequence)
+        {
+            foreach (char nucleotide in halfDNASequence)
+            {
+                if (!"ATCG".Contains(nucleotide))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static string ReplicateSeqeunce(string halfDNASequence)
+        {
+            string result = "";
+            foreach (char nucleotide in halfDNASequence)
+            {
+                result += "TAGC"["ATCG".IndexOf(nucleotide)];
+            }
+            return result;
+        }
+        static void output()
+        {
+            string halfDNASequence = Console.ReadLine();
+            if (IsValidSequence(halfDNASequence))
+            {
+                Console.WriteLine("Current half DNA sequence: " + halfDNASequence + "\n");
+                Console.WriteLine("Do you want to replicate it ? (Y/N) : ");
+                if (isinputValid())
+                {
+                    output();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Do you want to process another sequence ? (Y / N) : ");
+                if (isinputValid())
+                {
+                    output();
+                }
+                else
+                {
+                    Console.WriteLine("That half DNA sequence is invalid.");
+                    Console.WriteLine("Do you want to process another sequence ? (Y/N) : ");
+                    if (isinputValid())
+                    {
+                        output();
+                    }
+                }
+            }
+
+
+        }
+    }
+}
